893f34ceef92bc635422964b42139b67a7346742
schi12 <markus.schilde@springer.com>
2015-06-25 10:38:47 +0200

    Updated formatDate for OEC e-mail

--- src/com/artnology/sgw/alerting/helper/XmlFunctions.java
+++ src/com/artnology/sgw/alerting/helper/XmlFunctions.java
@@ -3,6 +3,7 @@ package com.artnology.sgw.alerting.helper;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class XmlFunctions {
 	public static String formatCurrentDate (String inputDateFormat) {
@@ -13,5 +14,16 @@ public class XmlFunctions {
 	
 	public static String cutOffAtLastDelimChar(String input, int maxLength) {
 		return input.substring(maxLength) + "_somemagic";
+	}
+	
+	public static String formatDate (String date, String inputDateFormat) {
+				
+		long millisecondsDate = Long.parseLong(date);
+		Calendar cal = Calendar.getInstance();
+		cal.setTimeInMillis(millisecondsDate);
+		
+		SimpleDateFormat dateFormat = new SimpleDateFormat(inputDateFormat);
+		
+		return dateFormat.format(cal.getTime());
 	}
 }
